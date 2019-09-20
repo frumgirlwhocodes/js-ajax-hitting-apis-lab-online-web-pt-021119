0c527d6fa5d2675b3c3f53d6a8c4b6b875dd6a75
lieby weintraub <liebahinda@gmail.com>
2019-09-20 19:28:56 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -11,8 +11,10 @@ function getRepositories() {
 function showRepositories(){
   const repos = JSON.parse(this.responseText)
   cosole.log(repos); 
-  const repoList= `<ul>${repos.map(r =>
-  }
+  const repoList= `<ul>${repos.map(r => `
   )
+  }
+  
+  
 
 }
