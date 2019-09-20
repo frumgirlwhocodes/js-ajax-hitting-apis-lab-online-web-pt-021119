60b9767935befb14bf1ac114e44f57727530bd7b
lieby weintraub <liebahinda@gmail.com>
2019-09-20 19:15:54 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function getRepositories() {
   const username= document.getElementByID('username').value
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
-  req.open('GET,')
+  req.open('GET', )
 }
 
 
